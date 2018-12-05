5139332a576c49e05607b0afb00240cbc95e18b8
alexmaes <alexandre.maes@ibpc.fr>
2018-12-05 10:21:19 +0100

    Update settings.py

--- nanodata/nanodata/settings.py
+++ nanodata/nanodata/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'fuy@5^h@$$)35nyr^1xml54vwpf2(6wf^do9v&dhd2a_y&j!t='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['harold.bmce.ibpc.fr']
+ALLOWED_HOSTS = ['harold.bmce.ibpc.fr', 'roarone.pythonanywhere.com']
 
 
 # Application definition
